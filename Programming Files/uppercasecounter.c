e8481d7b75dd69498afc709b646933332a68e7bc
Satyabrata Mohanty <92725722+Satya900@users.noreply.github.com>
2023-12-12 13:45:26 +0530

    Update uppercasecounter.c

--- Programming Files/uppercasecounter.c
+++ Programming Files/uppercasecounter.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+
+int countUpperCaseLetters(char *str) {
+    int count = 0;
+
+    // Iterate through each character in the string
+    while (*str != '\0') {
+        // Check if the current character is an uppercase letter
+        if (*str >= 'A' && *str <= 'Z') {
+            count++;
+        }
+
+        // Move to the next character in the string
+        str++;
+    }
+
+    return count;
+}
+
+int main() {
+    char inputString[100];
+
+    // Get input from the user
+    printf("Enter a string: ");
+    fgets(inputString, sizeof(inputString), stdin);
+
+    // Remove newline character from input
+    for (int i = 0; i < sizeof(inputString); i++) {
+        if (inputString[i] == '\n') {
+            inputString[i] = '\0';
+            break;
+        }
+    }
+
+    // Call the function to count uppercase letters
+    int result = countUpperCaseLetters(inputString);
+
+    // Display the result
+    printf("Number of uppercase letters: %d\n", result);
+
+    return 0;
+}
 
